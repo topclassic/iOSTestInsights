731d2beace2c2e15b148ac4d87fb7fdd0a845a96
Chotipat Top <topz.classic@gmail.com>
2016-06-21 15:06:52 +0700

    Initial Commit

---
+++ iOSTestInsights/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iOSTestInsights
+//
+//  Created by Digital-MacbookPro on 6/21/2559 BE.
+//  Copyright © 2559 Digital-MacbookPro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
